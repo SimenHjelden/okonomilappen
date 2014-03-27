5196187007577f1b2dda689af365dc8540a977c3
Magnus <kjemag12@nith.no>
2014-03-27 11:13:41 +0100

    I made some hardcore js, got mad skilz

---
+++ js/main.js
@@ -0,0 +1,59 @@
+(function() {
+	init();
+})();
+
+var menuWrap, mainContent, menuFlag, menuBtn;
+
+function setObjects() {
+	menuWrap = $( "#menuWrap" );
+	mainContent = $( "#mainContent" );
+	menuFlag = true;
+	menuBtn = $( "#menuBtn" );
+}
+
+function init() {
+	setObjects();
+	settEventHandlere();
+	setHeight();
+	setWidth();
+	setPosition();
+}
+
+$( window ).resize(function() {
+	setHeight();
+	setWidth();
+});
+
+function setPosition() {
+	menuWrap.css( "left", "0px" );
+	mainContent.css( "left", "200px" );
+}
+
+function setHeight() {
+	menuWrap.height( window.innerHeight );
+	mainContent.height( window.innerHeight );
+}
+
+function setWidth() {
+	mainContent.width( window.innerWidth - 200 );
+}
+
+function settEventHandlere() {
+	menuBtn.click(function(){
+		toggleMenu();
+	});
+}
+
+function toggleMenu() {
+	if(menuFlag) {
+		menuFlag = false;
+		menuWrap.css( "left", "-200px" );
+		mainContent.css( "left", "0px", "width" );
+	} else {
+		menuFlag = true;
+		menuWrap.css( "left", "0px" );
+		mainContent.css( "left", "200px" );
+	}
+	setHeight();
+	setWidth();
+}
